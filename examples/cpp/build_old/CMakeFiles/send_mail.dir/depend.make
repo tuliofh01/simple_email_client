a7e97ca64c49ac10b4b9ed762b83e600e8b40b4e
Túlio, the Duke. <70290204+tuliofh01@users.noreply.github.com>
2026-01-20 12:23:24 -0300

    Delete examples/cpp/build_old directory

--- examples/cpp/build_old/CMakeFiles/send_mail.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for send_mail.
-# This may be replaced when dependencies are built.
--- examples/cpp/build_old/CMakeFiles/send_mail.dir/src/main.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/send_mail.dir/src/main.cpp.o: \
- /home/tuliofh01/Documents/Carreira\ Profissional/Ocupações\ Atuais/Estágios/2026/ARSAE-MG/Arquivos/smtp_client/examples/cpp/src/main.cpp \
- /usr/include/stdc-predef.h
